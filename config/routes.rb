6d3ec46549fd7053a16c72e47bf2f404000a46da
Ukaza Perdana <hello@ukazap.space>
2019-12-13 15:44:21 +0800

    add maps controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'maps#index'
   post :receive, to: 'webhook#receive'
   get :receive, to: 'webhook#receive'
 end
